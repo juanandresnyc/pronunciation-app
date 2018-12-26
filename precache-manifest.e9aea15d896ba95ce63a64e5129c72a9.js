9996965717724f9444fbea1fa517141e8011de9f
juan andres andrango <andrango.juanandres@gmail.com>
2018-12-26 03:40:58 -0500

    Updates

---
+++ precache-manifest.e9aea15d896ba95ce63a64e5129c72a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e77cea4e150d5b49461",
+    "url": "/pronunciation-app/static/js/runtime~main.4e77cea4.js"
+  },
+  {
+    "revision": "fe598e4378a68e5c8acd",
+    "url": "/pronunciation-app/static/js/main.fe598e43.chunk.js"
+  },
+  {
+    "revision": "c4f47af46efe603326d9",
+    "url": "/pronunciation-app/static/js/1.c4f47af4.chunk.js"
+  },
+  {
+    "revision": "fe598e4378a68e5c8acd",
+    "url": "/pronunciation-app/static/css/main.1277a9fa.chunk.css"
+  },
+  {
+    "revision": "1c0c3de21c89b190854d12d4ec7d4e56",
+    "url": "/pronunciation-app/index.html"
+  }
+];
